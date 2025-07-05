693768af72161e761f47842382864ff86308facc
KBB <kadirberabilgin616161@gmail.com>
2025-07-05 17:45:56 +0300

    ProjectEuler 1. Soru
    ProjectEuler birinci sorusunun cevabı ekteki C# dosyasındadır.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// 10 dan küçük doğal sayılardan 3 veya 5 in katı olan sayılar : 3, 5, 6 ve 9 dur.
+// buna göre 1000 den küçük doğal sayıların 3 veya 5 in katı olan sayıları bul.
+namespace C_sharp_proje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    toplam += i;
+                }
+            }
+            Console.WriteLine("toplam : " + toplam); // cevap : 233168
+        }
+    }
+
+}
