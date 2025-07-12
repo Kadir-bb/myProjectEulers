a7946a1e23ffbe745310725b7aeb6562c26a25f3
KBB <kadirberabilgin616161@gmail.com>
2025-07-12 20:47:47 +0300

    Update Program10.cs
    bool dizisi fonksiyonun içinde oluşturulacak şekilde güncellendi

--- Program10.cs
+++ Program10.cs
@@ -7,32 +7,32 @@ namespace C_sharp_proje
         static void Main(string[] args)
         {
             int sayi = 2000000;
-            bool[] asalMi = new bool[sayi];
 
-            Console.WriteLine($"{sayi} sayısına kadar olan asal sayıların toplamı = {asalSayilarToplami(sayi, asalMi)}");
+            Console.WriteLine($"{sayi} sayısına kadar olan asal sayıların toplamı = {asalSayilarToplami(sayi)}");
         }
 
-        static long asalSayilarToplami(int sayi_, bool[] asalMi_)
+        static long asalSayilarToplami(int sayi_)
         {
+            bool[] asalMi = new bool[sayi_];
             for (int i = 2; i < sayi_; i++)
             {
-                asalMi_[i] = true; // 0 ve 1 hariç olan sayıları asal kabul edelim
+                asalMi[i] = true; // 0 ve 1 hariç olan sayıları asal kabul edelim
             }
 
             for (int i = 2; i * i < sayi_; i++)
             {
-                if (asalMi_[i])
+                if (asalMi[i])
                 {
                     for (int j = i * i; j < sayi_; j += i)
                     {
-                        asalMi_[j] = false;
+                        asalMi[j] = false;
                     }
                 }
             }
             long toplam = 0;
             for (int i = 2; i < sayi_; i++)
             {
-                if (asalMi_[i])
+                if (asalMi[i])
                     toplam += i;
             }
             return toplam;
